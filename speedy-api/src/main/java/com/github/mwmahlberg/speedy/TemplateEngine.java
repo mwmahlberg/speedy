08a765af43e25b38969686bbd107a76655216918
Markus W. Mahlberg <markus.mahlberg@me.com>
2014-10-18 18:50:46 +0200

    Allow the template engine to write to the output stream writer directly

--- speedy-api/src/main/java/com/github/mwmahlberg/speedy/TemplateEngine.java
+++ speedy-api/src/main/java/com/github/mwmahlberg/speedy/TemplateEngine.java
@@ -1,7 +1,10 @@
 package com.github.mwmahlberg.speedy;
 
+import java.io.Writer;
+
 public interface TemplateEngine {
 	
 	public String render(ModelAndView modelAndView);
 
+	public void render(ModelAndView modelAndView, Writer writer);
 }
