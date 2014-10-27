f2668a06110bd53e660e2a6de37e2cf0509c839a
Markus W. Mahlberg <markus.mahlberg@me.com>
2014-10-27 22:24:44 +0100

    Improved displaying of exceptions

--- speedy-core/src/main/java/com/github/mwmahlberg/speedy/handler/DefaultWebApplicationExceptionHandler.java
+++ speedy-core/src/main/java/com/github/mwmahlberg/speedy/handler/DefaultWebApplicationExceptionHandler.java
@@ -6,15 +6,23 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.sun.jersey.api.view.Viewable;
 
 @Singleton
 @Provider
 public class DefaultWebApplicationExceptionHandler implements
 		ExceptionMapper<WebApplicationException> {
+	
+	Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Override
 	public Response toResponse(WebApplicationException exception) {
+		
+		logger.warn("WebApplicationException cought!", exception);
+		
 		return Response.status(500)
 				.entity(new Viewable("_status/Default", exception)).build();
 	}
