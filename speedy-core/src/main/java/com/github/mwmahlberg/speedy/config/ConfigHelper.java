acb057cb88f63a4296ea19d68f9fd08eecff5e53
Markus W. Mahlberg <markus.mahlberg@me.com>
2014-10-28 16:54:54 +0100

    Make it deal with edge cases a bit better

--- speedy-core/src/main/java/com/github/mwmahlberg/speedy/config/ConfigHelper.java
+++ speedy-core/src/main/java/com/github/mwmahlberg/speedy/config/ConfigHelper.java
@@ -2,48 +2,60 @@ package com.github.mwmahlberg.speedy.config;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Optional;
+
+import static com.google.common.base.Preconditions.*;
+
 public class ConfigHelper {
-	
-	private static final Logger logger = LoggerFactory.getLogger(ConfigHelper.class);
-	
-	public static Properties getProperties(Set<String> props, Properties fileConfig){
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(ConfigHelper.class);
+
+	public static Properties getProperties(Set<String> props,
+			Properties fileConfig) throws IOException {
 		Properties defaults = new Properties();
 
 		Properties config = new Properties();
 
+		if (props == null) {
+			props = new HashSet<String>();
+		}
+
 		if (props.size() > 0) {
 			for (String string : props) {
 				logger.info("Processing {}", string);
-				try {
-					InputStream is = ConfigHelper.class.getClassLoader()
-							.getResourceAsStream(string);
 
-					if (string.startsWith("META-INF/_speedy")) {
-						defaults.load(is);
-						logger.info("Loaded default configuration from {}",
-								string);
-					} else {
-						config.load(is);
-						logger.info("Loaded configuration from {}", string);
-					}
+				InputStream is = ConfigHelper.class.getClassLoader().getResourceAsStream(string);
 
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				if (is == null) {
+					throw new IOException("Property file " + string
+							+ " not found or not readable");
+				} else if (string.startsWith("META-INF/_speedy")) {
+					defaults.load(is);
+					logger.info("Loaded default configuration from {}", string);
+				} else {
+					config.load(is);
+					logger.info("Loaded configuration from {}", string);
 				}
+
 			}
 		}
 
 		Properties appConfig = new Properties();
 		appConfig.putAll(defaults);
 		appConfig.putAll(config);
-		appConfig.putAll(fileConfig);
+
+		if (fileConfig != null) {
+			appConfig.putAll(fileConfig);
+		}
+
 		appConfig.putAll(System.getProperties());
 
 		logger.debug("defaults: {}", defaults);
